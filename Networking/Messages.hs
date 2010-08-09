8d1ffdb1e4ef061b6ec6d8de970de0eea8f827af
Benjamin Klüglein <scheibenkaes@googlemail.com>
2010-08-09 22:02:36 +0200

    Added show instance for ServerMessage

--- Networking/Messages.hs
+++ Networking/Messages.hs
@@ -14,7 +14,15 @@ data ServerMessage =
     | THRW Points Message
     | DEF MyPoints OtherPoints Message
     | WIN MyPoints OtherPoints Message
-    deriving (Show, Eq, Read)
+    deriving (Eq, Read)
+
+instance Show ServerMessage where
+    show (HELO v m) = "HELO " ++ v ++ " " ++ m
+    show (DENY m)   = "DENY " ++ m
+    show (TURN my other m) = "TURN " ++ show my ++ " " ++ show other ++ " " ++ m 
+    show (THRW p m) = "THRW " ++ show p ++ " " ++ m
+    show (DEF my other m) = "DEF " ++ show my ++ " " ++ show other ++ " " ++ m
+    show (WIN my other m) = "WIN " ++ show my ++ " " ++ show other ++ " " ++ m
 
 data ClientMessage = 
     AUTH Name Message
