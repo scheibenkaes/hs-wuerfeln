669d9444aac4b7894ff0f205660b1cdf001fcbe5
Benjamin Klüglein <scheibenkaes@googlemail.com>
2010-08-09 20:26:35 +0200

    Removed unneeded class and replaced it with a type

--- Game/Logic.hs
+++ Game/Logic.hs
@@ -5,8 +5,7 @@ import Game.Gameplay
 type Move = (PlayerChoice, Int)
 type Moves = [Move]
 
-class LogicFunc a where
-    calculateNextMove :: 
-        [a] -> -- Eigene Zuege
-        [a] -> -- Gegnerische Zuege
-        PlayerChoice -- Wuerfeln oder nicht wuerfeln, das ist hier die Frage
+type LogicCallback = ([Moves] -> [Moves] -> PlayerChoice)
+
+stupidLogic :: LogicCallback
+stupidLogic own other = ROLL
