0080dfdc861a0609f7322aa41b8aa5b4931cb9f8
Benjamin Klüglein <scheibenkaes@googlemail.com>
2010-08-09 20:27:53 +0200

    First connection impl based on rwh

--- Networking/Server.hs
+++ Networking/Server.hs
@@ -1,6 +1,26 @@
 module Networking.Server where 
 
+import Network.Socket
+import System.IO
+
 server = "wettbewerb.linux-magazin.de"
 
-port = 3333
+port = "3333"
+
+type ServerConnection = IO Handle
+
+connectToServer :: String -> String -> ServerConnection
+connectToServer url port = do
+    addrInfo <- getAddrInfo Nothing (Just url) (Just port)
+    let serverAddr = head addrInfo
+
+    sock <- socket (addrFamily serverAddr) Stream defaultProtocol
+    setSocketOption sock KeepAlive 1
+
+    connect sock (addrAddress serverAddr)
+
+    h <- socketToHandle sock WriteMode
+    hSetBuffering h LineBuffering
+
+    return h
 
