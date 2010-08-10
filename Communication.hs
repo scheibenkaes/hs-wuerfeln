beb9755f2d66f3d565998c86c41391c2d19d8149
Benjamin Klüglein <scheibenkaes@googlemail.com>
2010-08-10 20:25:57 +0200

    Removed trash

--- Communication.hs
+++ Communication.hs
@@ -5,11 +5,3 @@ import Data.List
 import Networking.Messages hiding (ROLL, SAVE)
 import Game.Gameplay
 
-convertChoiceToMessage :: PlayerChoice -> String -> String
-convertChoiceToMessage ROLL s = "ROLL " ++ s ++ "\n"
-convertChoiceToMessage SAVE s = "SAVE " ++ s ++ "\n"
-
-convertServerMessageToChoice :: String -> PlayerChoice
-convertServerMessageToChoice msg    
-    | "ROLL " `isPrefixOf` msg = ROLL
-    | "SAVE " `isPrefixOf` msg = SAVE
